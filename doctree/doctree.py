3d1e653dfb9fa63ab7237943f2be2290d2ce6edb
Albert Visser <albert@lemoncurry>
2013-08-08 11:31:03 +0200

    Python 3 aanpassing mbt import ook voor Python 2

--- doctree/doctree.py
+++ doctree/doctree.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import absolute_import
 ## from doctree_wx import main
-from doctree_qt import main
+from doctree.doctree_qt import main
 
 if __name__ == "__main__":
     main('MyMan.ini')
