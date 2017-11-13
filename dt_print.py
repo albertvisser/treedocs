d14fb9ac74e425214ff59dbfbc4dc5e7a270fe7c
Albert Visser <albert@lemoncurry>
2017-11-13 22:23:08 +0100

    additions/changes to startup program

--- dt_print.py
+++ dt_print.py
@@ -1,16 +1,20 @@
 #! /usr/bin/env python3
-"""output contents of DocTree data file to separate documents
+"""output contents of DocTree data file to one or more documents
 """
 import argparse
 from doctree.doctree_2print import main
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-f', '--file', dest='fname')
+    parser = argparse.ArgumentParser(description=__doc__, epilog="output files "
+                                    "are created in the same directory as the "
+                                    "input file and end with .out")
+    parser.add_argument('fname', help="name of DocTree file")
     parser.add_argument('-H', '--html', dest='as_html', action='store_true',
-                        help='leave contents as stored instead of parsed into plain text')
-    parser.add_argument('-v', '--verbose', dest='to_files', action='store_true',
-                        help='echo output instead of sent to file')
+                        help='leave contents as stored instead of parsed into '
+                             'plain text')
+    parser.add_argument('-s', '--separate', dest='to_files', action='store_true',
+                        help='write output to separate files instead of to a '
+                             'single one (filenames are generated)')
     args = parser.parse_args()
     main(args.fname, args.as_html, args.to_files)
