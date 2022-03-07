197b6aa11b949c4c5f29f429a1008fcd5f00248d
Albert Visser <albert.visser@gmail.com>
2022-03-07 22:19:49 +0100

    tests vereenvoudigd

--- test_pickle_dml.py
+++ test_pickle_dml.py
@@ -1,5 +1,6 @@
 import doctree.pickle_dml as dml
 
+FNAME = 'testfile.dtr'
 testdata = [{'Application': 'DocTree'},
             [[(0, [(1, [])])]],
             {0: 'this is one text', 1: 'this is another'},
@@ -7,29 +8,13 @@ testdata = [{'Application': 'DocTree'},
 
 
 def test_write_to_files():
-    filename = 'testfile'
     settings, views, itemdict, textpos = testdata
-    dml.write_to_files(filename, settings, views, itemdict, textpos)
-    data_read = '', [], {}, {}
-    for item in dml.read_dtree('testfile'):
-        if item['type'] == 'settings':
-            data_read[0] = item['data']
-        elif item['type'] == 'view':
-            data_read[1].append(item['data'])
-        elif item['type'] == 'textitem':
-            data_read[2][item['textid']] = item['data']
-            data_read[3][item['textid']] = item['textpos']
-    return data_read  # should be equal to testdata
+    dml.write_to_files(FNAME, settings, views, itemdict, textpos)
 
 
 def test_read_from_files():
-    filename = 'testfile'
-    if filename in dml.list_dtrees():
-        dml.clear_dtree(filename, recreate=True)
-    else:
-        dml.create_new_dtree(filename)
     settings, views, itemdict, textpos = testdata
-    dml.write_to_files(filename, settings, views, itemdict, textpos)
+    dml.write_to_files(FNAME, settings, views, itemdict, textpos)
     data = dml.read_from_files('testfile')
     assert data == testdata
 
