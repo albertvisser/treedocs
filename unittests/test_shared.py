70c3b4444631c21f4be919e84d6bfd60ce60a251
Albert Visser <albert.visser@gmail.com>
2024-06-09 10:50:42 +0200

    meer testmethodes

--- unittests/test_shared.py
+++ unittests/test_shared.py
@@ -3,6 +3,23 @@
 import doctree.shared as testee
 
 
+class MockTree:
+    "stub"
+    def __init__(self):
+        self.counter = 0
+    def getitemdata(self, item):
+        print(f"called Tree.getitemdata with arg '{item}'")
+        return 'item title', 'item key'
+    def getitemkids(self, item):
+        print(f"called Tree.getitemkids with arg '{item}'")
+        self.counter += 1
+        if self.counter == 1:
+            return ['kid1', 'kid2']
+        return []
+    def add_to_parent(self, *args):
+        print(f"called Tree.add_to_parent with args", args)
+        return 'new treeitem'
+
 def test_log(monkeypatch, capsys):
     """unittest for shared.log
     """
@@ -25,16 +42,35 @@ def test_log(monkeypatch, capsys):
                                        'information\n')
 
 
-def _test_getsubtree(monkeypatch, capsys):
+def test_getsubtree(monkeypatch, capsys):
     """unittest for shared.getsubtree
     """
-    testee.getsubtree(tree, item, itemlist=None)
+    tree = MockTree()
+    assert testee.getsubtree(tree, 'item') == (
+            ('item title', 'item key', [('item title', 'item key', []),
+                                        ('item title', 'item key', [])]),
+            ['item key', 'item key', 'item key'])
+    assert capsys.readouterr().out == ("called Tree.getitemdata with arg 'item'\n"
+                                       "called Tree.getitemkids with arg 'item'\n"
+                                       "called Tree.getitemdata with arg 'kid1'\n"
+                                       "called Tree.getitemkids with arg 'kid1'\n"
+                                       "called Tree.getitemdata with arg 'kid2'\n"
+                                       "called Tree.getitemkids with arg 'kid2'\n")
 
 
-def _test_putsubtree(monkeypatch, capsys):
+def test_putsubtree(monkeypatch, capsys):
     """unittest for shared.putsubtree
     """
-    testee.putsubtree(tree, parent, titel, key, subtree=None, pos=-1)
+    tree = MockTree()
+    subtree = [('subtitle1', 'subkey1', []), ('subtitle2', 'subkey2', [])]
+    assert testee.putsubtree(tree, 'parent', 'titel', 'key', subtree, pos=1) == 'new treeitem'
+    assert capsys.readouterr().out == (
+            "called Tree.add_to_parent with args ('key', 'titel', 'parent', 1)\n"
+            "called Tree.add_to_parent with args ('subkey1', 'subtitle1', 'new treeitem', -1)\n"
+            "called Tree.add_to_parent with args ('subkey2', 'subtitle2', 'new treeitem', -1)\n")
+    assert testee.putsubtree(tree, 'parent', 'titel', 'key', pos=1) == 'new treeitem'
+    assert capsys.readouterr().out == (
+            "called Tree.add_to_parent with args ('key', 'titel', 'parent', 1)\n")
 
 
 def test_get_imagenames(monkeypatch, capsys):
